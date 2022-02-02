00a70a27d4949fe36bdc865b1e20d35e97d4349d
Bhavyai Gupta <zbhavyai@gmail.com>
2022-02-01 23:27:16 -0700

    build: model to store tags of the book #1 #2 #3 #4

---
+++ src/main/java/com/budlib/api/model/Tag.java
@@ -0,0 +1,36 @@
+package com.budlib.api.model;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.List;
+import lombok.*;
+
+/**
+ * Represents the tags of books
+ */
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "tag")
+public class Tag implements Serializable {
+    /**
+     * Internal unique ID of the tag
+     */
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "tag_id")
+    private int tagId;
+
+    /**
+     * String representation of the tag
+     */
+    @Column(name = "tag_name")
+    private String tagName;
+
+    /**
+     * Books that has this tag
+     */
+    @ManyToMany(mappedBy = "tags")
+    private List<Book> books;
+}
