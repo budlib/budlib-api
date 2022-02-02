25c997c26e0e36997aa3ad0e5271ddcea0995fa9
Bhavyai Gupta <zbhavyai@gmail.com>
2022-02-01 23:26:25 -0700

    build: storing class code separately from loaner

---
+++ src/main/java/com/budlib/api/model/ClassCode.java
@@ -0,0 +1,33 @@
+package com.budlib.api.model;
+
+import javax.persistence.*;
+import java.util.List;
+import java.io.Serializable;
+import lombok.*;
+
+/**
+ * Represents a class in the school
+ */
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@Table(name = "class_code")
+public class ClassCode implements Serializable {
+    /**
+     * Unique ID of the class
+     */
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "class_id")
+    private int classCodeId;
+
+    /**
+     * Class code or class name
+     */
+    @Column(name = "class_code")
+    private String classCode;
+
+    @ManyToMany(mappedBy = "classCode")
+    private List<Loaner> facultyList;
+}
