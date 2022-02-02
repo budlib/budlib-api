66e60acd4c48a9165f6eb55be28e26fb91069721
Bhavyai Gupta <zbhavyai@gmail.com>
2022-02-01 23:24:32 -0700

    build: model to store quantities of book in library #1 #2 #3 #4

---
+++ src/main/java/com/budlib/api/model/BookQuantity.java
@@ -0,0 +1,43 @@
+package com.budlib.api.model;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import lombok.*;
+
+/**
+ * Represents quantities of the Book
+ */
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@Table(name = "book_quantity")
+public class BookQuantity implements Serializable {
+    /**
+     * The book
+     */
+    @Id
+    @ManyToOne
+    @JoinColumn(name = "book_id", foreignKey = @ForeignKey(name = "fk_quantity_book"))
+    private Book bookId;
+
+    /**
+     * The branch of the library
+     */
+    @Id
+    @ManyToOne
+    @JoinColumn(name = "branch_id", foreignKey = @ForeignKey(name = "fk_quantity_branch"))
+    private LibraryBranch branchId;
+
+    /**
+     * Total quantity at the library branch
+     */
+    @Column(name = "total_quantity")
+    private int totalQuantity;
+
+    /**
+     * Available quantity at the library branch
+     */
+    @Column(name = "available_quantity")
+    private int available;
+}
