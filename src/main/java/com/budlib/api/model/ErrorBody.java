84f7c2b08c2dc5ef23746bb5ff354374ddea57e0
Bhavyai Gupta <zbhavyai@gmail.com>
2022-02-01 23:14:51 -0700

    build: using lombok to reduce boilerplate code

--- src/main/java/com/budlib/api/model/ErrorBody.java
+++ src/main/java/com/budlib/api/model/ErrorBody.java
@@ -1,38 +1,22 @@
 package com.budlib.api.model;
 
 import org.springframework.http.HttpStatus;
+import lombok.*;
 
 /**
  * Wraps a custom message as object to be presented during bad requests
  */
+@Getter
+@Setter
+@AllArgsConstructor
 public class ErrorBody {
+    /**
+     * HTTP Status of the bad request
+     */
     private HttpStatus status;
-    private String message;
 
     /**
-     * Constructor
-     *
-     * @param code    the bad http status code
-     * @param message the message to provide explanation
+     * The message to provide explanation
      */
-    public ErrorBody(HttpStatus code, String message) {
-        this.status = code;
-        this.message = message;
-    }
-
-    public HttpStatus getStatus() {
-        return this.status;
-    }
-
-    public void setStatus(HttpStatus status) {
-        this.status = status;
-    }
-
-    public String getMessage() {
-        return this.message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
+    private String message;
 }
