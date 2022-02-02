613e08b3185c458c004b9c7d25151375b60fed5a
Bhavyai Gupta <zbhavyai@gmail.com>
2022-02-02 03:31:35 -0700

    build: updating controller for librarian #11 #12

--- src/main/java/com/budlib/api/controller/LibrarianController.java
+++ src/main/java/com/budlib/api/controller/LibrarianController.java
@@ -1,89 +1,175 @@
 package com.budlib.api.controller;
 
-import java.net.URI;
-import java.util.Collection;
-import java.util.stream.Collectors;
-import com.budlib.api.model.Librarian;
-import com.budlib.api.repository.LibrarianRepository;
+import com.budlib.api.model.*;
+import com.budlib.api.repository.*;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-@RestController
-@RequestMapping("/librarianrepo")
 @CrossOrigin
+@RestController
+@RequestMapping("api/librarian")
 public class LibrarianController {
     @Autowired
     LibrarianRepository librarianRepository;
 
-    @GetMapping(value = "/librarians/{id}", produces = { "application/json", "application/xml" })
-    public ResponseEntity<Librarian> getStudentById(@PathVariable long id) {
-        Librarian p = librarianRepository.findById(id).get();
-        if (p == null)
-            return ResponseEntity.notFound().build();
-        else
-            return ResponseEntity.ok().body(p);
-    }
+    /**
+     * Search the librarian by id
+     *
+     * @param id librarian id
+     * @return list of librarians with id; the list would have utmost one element
+     */
+    private List<Librarian> searchLibrarianById(Long id) {
+        Optional<Librarian> librarianOptional = this.librarianRepository.findById(id);
 
-    @GetMapping(value = "/librarians/user/{username}", produces = { "application/json", "application/xml" })
-    public ResponseEntity<Librarian> getLibByusername(@PathVariable String username) {
-        Librarian p = librarianRepository.findByuserName(username).get(0);
-        if (p == null)
-            return ResponseEntity.notFound().build();
-        else
-            return ResponseEntity.ok().body(p);
-    }
-
-    @GetMapping(value = "/librarians", produces = { "application/json", "application/xml" })
-    public ResponseEntity<Collection<Librarian>> getBooks() {
-        Collection<Librarian> librarian = librarianRepository.findAll().stream()
-                .collect(Collectors.toList());
-        System.out.println("Number of Librarians: " + librarian.size());
-        return ResponseEntity.ok().body(librarian);
-    }
-
-    @PostMapping(value = "/addlibrarians", consumes = { "application/json", "application/xml" }, produces = {
-            "application/json", "application/xml" })
-    public ResponseEntity<?> insertProduct(@RequestBody Librarian librarian) {
-        if (librarianRepository.findByuserName(librarian.getUserName()).size() > 0) {
-            return (ResponseEntity<?>) ResponseEntity.status(409);
+        if (librarianOptional.isPresent()) {
+            List<Librarian> searchResults = new ArrayList<>();
+            searchResults.add(librarianOptional.get());
+            return searchResults;
         }
-        librarian.setId(librarianRepository.count() + 1);
-        librarianRepository.save(librarian);
-        URI uri = URI.create("/librarianrepo/librarians/" + librarian.getId());
-        return ResponseEntity.created(uri).body(librarian);
-    }
 
-    @PutMapping(value = "/updatelibrarians/{id}", consumes = { "application/json", "application/xml" })
-    public ResponseEntity<Void> updateProduct(@PathVariable long id, @RequestBody Librarian librarian) {
-        if (!librarianRepository.existsById(id))
-            return ResponseEntity.notFound().build();
         else {
-            librarian.setUserName(librarianRepository.findById(id).get().getUserName());
-            librarianRepository.save(librarian);
-            return ResponseEntity.ok().build();
-
+            return null;
         }
-
     }
 
-    @DeleteMapping("/librarians/{id}")
-    public ResponseEntity<Void> deleteProduct(@PathVariable long id) {
-        Librarian librarian = librarianRepository.findById(id).get();
-        if (librarian == null)
-            return ResponseEntity.notFound().build();
-        else {
-            librarianRepository.delete(librarian);
-            return ResponseEntity.ok().build();
+    /**
+     * Search the librarians by their email
+     *
+     * @param alllibrarians list of all librarians
+     * @param sT            search term
+     * @return filtered list of librarian with email meeting the search term
+     */
+    private List<Librarian> searchLibrarianByEmail(List<Librarian> allLibrarians, String sT) {
+        String searchTerm = sT.toLowerCase();
+        List<Librarian> searchResults = new ArrayList<>();
+
+        for (Librarian eachLibrarian : allLibrarians) {
+            if (eachLibrarian.getEmail() != null && eachLibrarian.getEmail().toLowerCase().contains(searchTerm)) {
+                searchResults.add(eachLibrarian);
+            }
         }
 
+        return searchResults;
+    }
+
+    /**
+     * Endpoint for GET - fetch librarian by id
+     *
+     * @param id librarian id
+     * @return librarian
+     */
+    @GetMapping(path = "{librarianId}")
+    public ResponseEntity<?> getLibrarianById(@PathVariable("librarianId") Long id) {
+        List<Librarian> l = this.searchLibrarianById(id);
+
+        if (l == null) {
+            String message = "Librarian not found";
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ErrorBody(HttpStatus.NOT_FOUND, message));
+        }
+
+        else {
+            return ResponseEntity.status(HttpStatus.OK).body(l.get(0));
+        }
+    }
+
+    /**
+     * Endpoint for GET - search and fetch all librarians meeting search criteria
+     *
+     * @param searchBy   where to search
+     * @param searchTerm what to search
+     * @return list of librarians meeting search criteria
+     */
+    @GetMapping()
+    public ResponseEntity<?> searchLibrarian(@RequestParam(name = "searchBy", required = false) String searchBy,
+            @RequestParam(name = "searchTerm", required = false) String searchTerm) {
+
+        List<Librarian> allLibrarians = this.librarianRepository.findAll();
+
+        if (searchBy == null || searchTerm == null) {
+            return ResponseEntity.status(HttpStatus.OK).body(allLibrarians);
+        }
+
+        else if (searchBy.equals("") || searchTerm.equals("")) {
+            return ResponseEntity.status(HttpStatus.OK).body(allLibrarians);
+        }
+
+        else if (searchBy.equalsIgnoreCase("id")) {
+            return ResponseEntity.status(HttpStatus.OK).body(this.searchLibrarianById(Long.valueOf(searchTerm)));
+        }
+
+        else if (searchBy.equalsIgnoreCase("email")) {
+            return ResponseEntity.status(HttpStatus.OK).body(this.searchLibrarianByEmail(allLibrarians, searchTerm));
+        }
+
+        else {
+            String message = "Invalid librarian search operation";
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorBody(HttpStatus.BAD_REQUEST, message));
+        }
+    }
+
+    /**
+     * Endpoint for POST - save the librarian in db
+     *
+     * @param l librarian details in json
+     * @return the message
+     */
+    @PostMapping
+    public ResponseEntity<?> addlibrarian(@RequestBody Librarian l) {
+        // reset the id to 0 to prevent overwrite
+        l.setLibrarianId(0L);
+
+        this.librarianRepository.save(l);
+
+        String message = "Librarian added successfully";
+        return ResponseEntity.status(HttpStatus.OK).body(new ErrorBody(HttpStatus.OK, message));
+    }
+
+    /**
+     * Endpoint for PUT - update librarian in db
+     *
+     * @param l           the updated librarian details in json
+     * @param librarianId the id of the librarian to be updated
+     * @return the message
+     */
+    @PutMapping(path = "{librarianId}")
+    public ResponseEntity<?> updatelibrarian(@RequestBody Librarian l, @PathVariable("librarianId") Long librarianId) {
+        Optional<Librarian> librarianOptional = this.librarianRepository.findById(librarianId);
+
+        if (librarianOptional.isPresent()) {
+            l.setLibrarianId(librarianId);
+            this.librarianRepository.save(l);
+
+            String message = "Librarian updated successfully";
+            return ResponseEntity.status(HttpStatus.OK).body(new ErrorBody(HttpStatus.OK, message));
+        }
+
+        else {
+            String message = "Librarian not found";
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ErrorBody(HttpStatus.NOT_FOUND, message));
+        }
+    }
+
+    /**
+     * Endpoint for DELETE - delete librarian from db
+     *
+     * @param librarianId librarian id
+     */
+    @DeleteMapping(path = "{librarianId}")
+    public ResponseEntity<?> deletelibrarian(@PathVariable("librarianId") Long librarianId) {
+        if (!this.librarianRepository.existsById(librarianId)) {
+            String message = "Librarian not found";
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ErrorBody(HttpStatus.NOT_FOUND, message));
+        }
+
+        else {
+            this.librarianRepository.deleteById(librarianId);
+            String message = "Librarian deleted successfully";
+            return ResponseEntity.status(HttpStatus.OK).body(new ErrorBody(HttpStatus.OK, message));
+        }
     }
 }
