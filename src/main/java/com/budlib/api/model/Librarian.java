a54593964e71a4d4039f3608d52548bf34debc61
Bhavyai Gupta <zbhavyai@gmail.com>
2022-02-01 23:16:54 -0700

    build: specifying field names and #13

--- src/main/java/com/budlib/api/model/Librarian.java
+++ src/main/java/com/budlib/api/model/Librarian.java
@@ -1,107 +1,67 @@
 package com.budlib.api.model;
 
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.io.Serializable;
+import lombok.*;
 
-public class Librarian {
-
+/**
+ * Represents the library coordinator
+ */
+@Entity
+@Getter
+@Setter
+@Table(name = "librarian")
+public class Librarian implements Serializable {
+    /**
+     * Internal unique ID of the library coordinator
+     */
     @Id
-    private Long id;
-    private String userName;
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "librarian_id")
+    private Long librarianId;
+
+    /**
+     * First name of the coordinator
+     */
+    @Column(name = "first_name")
     private String firstName;
+
+    /**
+     * Middle name of the coordinator
+     */
+    @Column(name = "middle_name")
+    private String middleName;
+
+    /**
+     * Last name of the coordinator
+     */
+    @Column(name = "last_name")
     private String lastName;
+
+    /**
+     * Login username of the coordinator
+     */
+    @Column(name = "username")
+    private String userName;
+
+    /**
+     * Email of the coordinator
+     */
+    @Column(name = "email")
+    private String email;
+
+    /**
+     * Login password of the coordinator
+     * TODO: replace with encrypted string
+     */
+    @Column(name = "password")
     private String password;
+
+    /**
+     * Role in the library
+     */
+    @Column(name = "role")
     private String role;
 
-    public Librarian(Long id, String userName, String firstName, String lastName, String password, String role) {
-        this.id = id;
-        this.userName = userName;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.role = role;
-        this.password = password;
-    }
-
-    /**
-     * @return String return the userName
-     */
-    public String getUserName() {
-        return userName;
-    }
-
-    /**
-     * @param userName the userName to set
-     */
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
-    /**
-     * @return String return the firstName
-     */
-    public String getFirstName() {
-        return firstName;
-    }
-
-    /**
-     * @param firstName the firstName to set
-     */
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    /**
-     * @return String return the lastName
-     */
-    public String getLastName() {
-        return lastName;
-    }
-
-    /**
-     * @param lastName the lastName to set
-     */
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    /**
-     * @return String return the password
-     */
-    public String getPassword() {
-        return password;
-    }
-
-    /**
-     * @param password the password to set
-     */
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    /**
-     * @return String return the role
-     */
-    public String getRole() {
-        return role;
-    }
-
-    /**
-     * @param role the role to set
-     */
-    public void setRole(String role) {
-        this.role = role;
-    }
-
-    /**
-     * @return Long return the id
-     */
-    public Long getId() {
-        return id;
-    }
-
-    /**
-     * @param id the id to set
-     */
-    public void setId(Long id) {
-        this.id = id;
-    }
+    // TODO: add branch wise coordinator and a superadmin
 }
