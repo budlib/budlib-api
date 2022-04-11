6f07a38d6622cdd79de754f5d3fa6767d9f18f8e
Bhavyai Gupta <zbhavyai@gmail.com>
2022-04-10 19:18:36 -0600

    fix: fixing harmless warnings

--- src/main/java/com/budlib/api/model/TrnQuantitiesId.java
+++ src/main/java/com/budlib/api/model/TrnQuantitiesId.java
@@ -20,4 +20,27 @@ public class TrnQuantitiesId implements Serializable {
      * The books involved in the transaction
      */
     private Long book;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+
+        if (o == null || this.getClass() != o.getClass())
+            return false;
+
+        TrnQuantitiesId that = (TrnQuantitiesId) o;
+
+        if (this.transaction != null ? !transaction.equals(that.transaction) : that.transaction != null)
+            return false;
+
+        return this.book != null ? book.equals(that.book) : that.book == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = this.transaction != null ? this.transaction.hashCode() : 0;
+        result = 31 * result + (this.book != null ? this.book.hashCode() : 0);
+        return result;
+    }
 }
--- src/main/java/com/budlib/api/repository/TrnQuantitiesRepository.java
+++ src/main/java/com/budlib/api/repository/TrnQuantitiesRepository.java
@@ -2,10 +2,10 @@ package com.budlib.api.repository;
 
 import com.budlib.api.model.TrnQuantities;
 import com.budlib.api.model.TrnQuantitiesId;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TrnQuantitiesRepository extends CrudRepository<TrnQuantities, TrnQuantitiesId> {
+public interface TrnQuantitiesRepository extends JpaRepository<TrnQuantities, TrnQuantitiesId> {
 
 }
