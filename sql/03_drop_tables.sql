0d50d98e2c4e4fbab701aa7961d1e21ff2b25b61
Bhavyai Gupta <zbhavyai@gmail.com>
2022-05-20 23:24:25 -0600

    build: adding drop table sequence

---
+++ sql/03_drop_tables.sql
@@ -0,0 +1,9 @@
+-- dropping the tables in this order
+DROP TABLE book_tag;
+DROP TABLE tag;
+DROP TABLE loan;
+DROP TABLE trn_quantities;
+DROP TABLE transaction;
+DROP TABLE librarian;
+DROP TABLE loaner;
+DROP TABLE book;
