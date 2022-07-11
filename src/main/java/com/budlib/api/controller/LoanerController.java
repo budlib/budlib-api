ca9e82bf7957fc8db72dab203a5e2eaa9a1a8dfb
Bhavyai Gupta <zbhavyai@gmail.com>
2022-07-10 20:04:01 -0600

    build: new import method for loaners

--- src/main/java/com/budlib/api/controller/LoanerController.java
+++ src/main/java/com/budlib/api/controller/LoanerController.java
@@ -1,6 +1,11 @@
 package com.budlib.api.controller;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Optional;
 
@@ -11,6 +16,9 @@ import com.budlib.api.repository.LoanerRepository;
 import com.budlib.api.repository.TransactionRepository;
 import com.budlib.api.response.ErrorBody;
 
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -328,42 +336,196 @@ public class LoanerController {
     }
 
     /**
-     * Endpoint for POST - import multiple loaners in db
+     * Verifies if the CSV record if empty or not. A record is empty when all the
+     * intended details are blank.
      *
-     * @param l list of loaner details in json
-     * @return the message
+     * @param record The CSV record to check for emptiness
+     * @return {@code true} if empty, {@code false} false otherwise
+     */
+    private boolean checkEmptyRecord(CSVRecord record) {
+
+        LOGGER.info("checkEmptyRecord: record = {}", record);
+
+        Iterator<String> itr = record.iterator();
+
+        while (itr.hasNext()) {
+            String str = itr.next();
+
+            if (str != null && str.length() != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    /**
+     * Converts CSV record to Loaner object, with the field mapping provided by
+     * loanerCsv
+     *
+     * @param record    The CSV Record fetched from the CSV file
+     * @param loanerCsv
+     * @return book object
+     * @throws NumberFormatException
+     * @throws NullPointerException
+     */
+    private Loaner csvRecordToLoaner(final CSVRecord record, HashMap<String, String> loanerCsv)
+            throws NumberFormatException, NullPointerException {
+
+        LOGGER.info("csvRecordToLoaner: record = {}, loanerCsv map = {}", record, loanerCsv);
+
+        if (this.checkEmptyRecord(record)) {
+            return null;
+        }
+
+        Loaner l = new Loaner();
+        l.setLoanerId(0);
+
+        // all the below are actually integers in String format
+        String schoolId = loanerCsv.get("schoolId");
+        String isStudent = loanerCsv.get("isStudent");
+        String email = loanerCsv.get("email");
+        String salutation = loanerCsv.get("salutation");
+        String firstName = loanerCsv.get("first_name");
+        String middleName = loanerCsv.get("middle_name");
+        String lastName = loanerCsv.get("last_name");
+        String motherName = loanerCsv.get("mother_name");
+        String fatherName = loanerCsv.get("father_name");
+
+        // set school ID
+        if (schoolId != null) {
+            l.setSchoolId(record.get(Integer.parseInt(schoolId)));
+        }
+
+        // set isStudent
+        if (isStudent != null) {
+            l.setStudent(Boolean.valueOf(record.get(Integer.parseInt(isStudent))));
+        }
+
+        // set email
+        if (email != null) {
+            l.setEmail(record.get(Integer.parseInt(email)));
+        }
+
+        // set salutation
+        if (salutation != null) {
+            l.setSalutation(record.get(Integer.parseInt(salutation)));
+        }
+
+        // set first name
+        if (firstName != null) {
+            l.setFirstName(record.get(Integer.parseInt(firstName)));
+        }
+
+        // set middle name
+        if (middleName != null) {
+            l.setMiddleName(record.get(Integer.parseInt(middleName)));
+        }
+
+        // set last name
+        if (lastName != null) {
+            l.setLastName(record.get(Integer.parseInt(lastName)));
+        }
+
+        // set mother name
+        if (motherName != null) {
+            l.setMotherName(record.get(Integer.parseInt(motherName)));
+        }
+
+        // set father name
+        if (fatherName != null) {
+            l.setFatherName(record.get(Integer.parseInt(fatherName)));
+        }
+
+        return l;
+    }
+
+    /**
+     * Import the CSV file that was uploaded, with the help of fields mapping
+     * provided
+     *
+     * @param loanerCsv the fields mapping
+     * @return message
      */
     @PostMapping(path = "import")
-    public ResponseEntity<?> importLoaners(@RequestBody List<Loaner> loanerList) {
+    public ResponseEntity<?> importLoaners(@RequestBody HashMap<String, String> loanerCsv) {
 
-        LOGGER.info("importLoaners: loanerList = {}", loanerList);
+        LOGGER.info("importLoaners: loanerCsv map = {}", loanerCsv);
 
-        boolean flag = false;
-        int countNotImported = 0;
         int countImported = 0;
+        final String uploadCSVFilePath = loanerCsv.get("filename");
 
-        for (Loaner eachLoaner : loanerList) {
-            // reset the id to 0 to prevent overwrite
-            eachLoaner.setLoanerId(0L);
+        // remove the filename as its not needed anymore, and to prevent hindrance
+        loanerCsv.remove("filename");
 
-            if (this.checkLoanerUniqueness(eachLoaner)) {
-                this.loanerRepository.save(eachLoaner);
-                countImported++;
+        try {
+            FileReader fileReader = new FileReader(uploadCSVFilePath);
+
+            CSVFormat csvFormat = CSVFormat.DEFAULT.withHeader();
+
+            CSVParser csvParser = new CSVParser(fileReader, csvFormat);
+            Iterator<CSVRecord> itr = csvParser.iterator();
+
+            while (itr.hasNext()) {
+                CSVRecord record = itr.next();
+
+                Loaner l = this.csvRecordToLoaner(record, loanerCsv);
+
+                if (l == null) {
+                    continue;
+                }
+
+                else if (this.checkLoanerUniqueness(l) == false) {
+                    LOGGER.warn("importLoaners: Skipping importing {} loaner as its not unique", l.getSchoolId());
+                }
+
+                else {
+                    this.loanerRepository.save(l);
+                    countImported++;
+                }
             }
 
-            else {
-                flag = true;
-                countNotImported++;
-            }
+            csvParser.close();
+
+            String message = String.format("%d loaners imported successfully", countImported);
+            return ResponseEntity.status(HttpStatus.OK).body(new ErrorBody(HttpStatus.OK, message));
+
         }
 
-        String message = String.format("%d loaners imported successfully", countImported);
-
-        if (flag) {
-            message += String.format("\n%d duplicate loaners not imported", countNotImported);
+        catch (FileNotFoundException e) {
+            String message = "Error in retrieving uploaded file";
+            LOGGER.error(message, e);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(new ErrorBody(HttpStatus.INTERNAL_SERVER_ERROR, message));
         }
 
-        return ResponseEntity.status(HttpStatus.OK).body(new ErrorBody(HttpStatus.OK, message));
+        catch (NumberFormatException | ArrayIndexOutOfBoundsException e) {
+            String message = "React Coding issue: Incorrect mapping provided";
+            LOGGER.error(message, e);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(new ErrorBody(HttpStatus.INTERNAL_SERVER_ERROR, message));
+        }
+
+        catch (IOException e) {
+            String message = "Error in parsing uploaded file";
+            LOGGER.error(message, e);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(new ErrorBody(HttpStatus.INTERNAL_SERVER_ERROR, message));
+        }
+
+        catch (NullPointerException e) {
+            String message = "Coding issue: All nulls are not handled";
+            LOGGER.error(message, e);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(new ErrorBody(HttpStatus.INTERNAL_SERVER_ERROR, message));
+        }
+
+        catch (Exception e) {
+            String message = "Something unexpected happened";
+            LOGGER.error(message, e);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(new ErrorBody(HttpStatus.INTERNAL_SERVER_ERROR, message));
+        }
     }
 
     /**
